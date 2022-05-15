7cbf1d1cd63e2c6ee39542c60f8e662de9986626
Grandes <ho2708@naver.com>
2022-05-15 15:53:57 +0900

    feat(spring clone): spring web clone
    spring web clone

---
+++ travelclub-spring-web/src/main/java/io/namoosori/travelclub/web/store/jpastore/jpo/TravelClubJpo.java
@@ -0,0 +1,38 @@
+package io.namoosori.travelclub.web.store.jpastore.jpo;
+
+import io.namoosori.travelclub.web.aggregate.club.TravelClub;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.BeanUtils;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.TreeMap;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name="TRAVEL_CLUB")
+public class TravelClubJpo {
+
+    @Id
+    private String id;
+    private String name;
+    private String intro;
+    private long foundationTime;
+
+    public TravelClubJpo(TravelClub travelClub) {
+        BeanUtils.copyProperties(travelClub, this);
+    }
+
+    public TravelClub toDomain() {
+        TravelClub travelClub = new TravelClub(this.name, this.intro);
+        travelClub.setId(this.id);
+        travelClub.setFoundationTime(this.foundationTime);
+        return travelClub;
+    }
+
+}
